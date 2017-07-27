ab4bea5d8c9a91a4649c60750e5fbb31296f507a
gejun <gejun@baidu.com>
2017-07-27 23:50:43 +0800

    find accurate incpaths from --incs
    Change-Id: I0dcd814d3671a5284442749578a5b4f2da5cbc2a

--- config_brpc.sh
+++ config_brpc.sh
@@ -41,20 +41,33 @@ find_lib_or_die() {
 find_dir_of_header() {
     find ${INCS} -path "*/$1" | sed "s|$1||g"
 }
+find_dir_of_header_or_die() {
+    local dir=$(find_dir_of_header $1)
+    if [ -z "$dir" ]; then
+        >&2 echo "fail to find $1 from -incs"
+        exit 1
+    else
+        echo $dir
+    fi
+}
 
 GFLAGS_LIB=$(find_lib_or_die libgflags.a)
 PROTOBUF_LIB=$(find_lib_or_die libprotobuf.a)
 PROTOC_LIB=$(find_lib_or_die libprotoc.a)
-PROTOC=$(which protoc)
+PROTOC=$(which protoc 2>/dev/null)
 if [ -z "$PROTOC" ]; then
     PROTOC=$(find_lib_or_die protoc)
 fi
-PROTOBUF_INC=$(find_dir_of_header google/protobuf/message.h)
 LEVELDB_LIB=$(find_lib_or_die libleveldb.a)
-SNAPPY_LIB=$(find_lib_or_die libsnappy.a)
+SNAPPY_LIB=$(find_lib libsnappy.a)
+
+GFLAGS_INC=$(find_dir_of_header gflags/gflags.h)
+PROTOBUF_INC=$(find_dir_of_header google/protobuf/message.h)
+LEVELDB_INC=$(find_dir_of_header leveldb/db.h)
+NEW_INCS=$(echo $GFLAGS_INC $PROTOBUF_INC $LEVELDB_INC | sort | uniq)
 
 #can't use \n in texts because sh does not support -e
-echo "INCS=$INCS" > config.mk
+echo "INCS=$NEW_INCS" > config.mk
 echo "LIBS=$GFLAGS_LIB $PROTOBUF_LIB $LEVELDB_LIB $SNAPPY_LIB" >> config.mk
 echo "PROTOC_LIB=$PROTOC_LIB" >> config.mk
 echo "PROTOC=$PROTOC" >> config.mk
@@ -65,6 +78,10 @@ echo "ifeq (\$(LINK_PERFTOOLS), 1)" >> config.mk
 TCMALLOC_LIB=$(find_lib libtcmalloc_and_profiler.a)
 if [ ! -z "$TCMALLOC_LIB" ]; then
     echo "    LIBS+=$TCMALLOC_LIB" >> config.mk
+    TCMALLOC_INC=$(find_dir_of_header google/tcmalloc.h)
+    if [ ! -z "$TCMALLOC_INC" ] && [ "$TCMALLOC_INC" != "$INCS" ]; then
+        echo "    INCS+=$TCMALLOC_INC" >> config.mk
+    fi
 fi
 UNWIND_LIB=$(find_lib libunwind.a)
 if [ ! -z "$UNWIND_LIB" ]; then
