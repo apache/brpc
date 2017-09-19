2cf16de47b557f1a86cbf61de9b36c60be7df794
gejun <gejun@baidu.com>
2017-09-19 17:01:00 +0800

    Avoid concurrent submake in makefile of UT

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -9,18 +9,18 @@ for test_bin in $test_bins; do
     >&2 echo "[runtest] $test_bin"
     ./$test_bin
     rc=$?
-    if [[ $rc -ne 0 ]]; then
+    if [ $rc -ne 0 ]; then
         failed_test="$test_bin"
         break;
     fi
 done
-if [[ $test_num -eq 0 ]]; then
+if [ $test_num -eq 0 ]; then
     >&2 echo "[runtest] Cannot find any tests"
     exit 1
 fi
-if [[ -z "$failed_test" ]]; then
+if [ -z "$failed_test" ]; then
     >&2 echo "[runtest] $test_num succeeded"
-elif [[ $test_num -gt 1 ]]; then
+elif [ $test_num -gt 1 ]; then
     >&2 echo "[runtest] '$failed_test' failed, $((test_num-1)) succeeded"
 else
     >&2 echo "[runtest] '$failed_test' failed"
