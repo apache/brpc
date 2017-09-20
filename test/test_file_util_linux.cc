cb7e05d74a9dc00b6b723aeee1c8f069397287cb
old-bear <jrjbear@gmail.com>
2017-09-20 11:42:41 +0800

    Remove a non-exist include in UT

--- test/test_file_util_linux.cc
+++ test/test_file_util_linux.cc
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "test/test_file_util.h"
-
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
