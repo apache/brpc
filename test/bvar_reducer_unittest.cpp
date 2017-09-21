74abdd831d8729b4900ad41a984c2556cb692d4c
Zhangyi Chen <frozen.zju@gmail.com>
2017-09-21 11:58:45 +0800

    Fix the failure of FileUtilTest when the user is root

--- test/bvar_reducer_unittest.cpp
+++ test/bvar_reducer_unittest.cpp
@@ -276,7 +276,7 @@ static void* string_appender(void* arg) {
     int count = 0;
     std::string id = butil::string_printf("%lld", (long long)pthread_self());
     std::string tmp = "a";
-    for (count = 0; !g_stop; ++count) {
+    for (count = 0; !count || !g_stop; ++count) {
         *cater << id << ":";
         for (char c = 'a'; c <= 'z'; ++c) {
             tmp[0] = c;
