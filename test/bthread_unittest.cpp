99ce6d2d7b3868bcf485d1a8a3b54e71899be866
gejun <gejun@baidu.com>
2017-09-29 19:53:44 +0800

    fix UT which is broken by incorrect patch

--- test/bthread_unittest.cpp
+++ test/bthread_unittest.cpp
@@ -506,21 +506,16 @@ TEST_F(BthreadTest, too_many_nosignal_threads) {
         ASSERT_EQ(0, bthread_start_urgent(&tid, &attr, dummy_thread, NULL));
     }
 }
-} // namespace
 
-int main(int argc, char** argv) {
-    testing::InitGoogleTest(&argc, argv);
-    GFLAGS_NS::ParseCommandLineFlags(&argc, &argv, true);
-    return RUN_ALL_TESTS();
-}
-
-void* yield_thread(void*) {
+static void* yield_thread(void*) {
     bthread_yield();
     return NULL;
 }
 
-TEST_F(test_bthread_suite, yield_single_thread) {
+TEST_F(BthreadTest, yield_single_thread) {
     bthread_t tid;
     ASSERT_EQ(0, bthread_start_background(&tid, NULL, yield_thread, NULL));
     ASSERT_EQ(0, bthread_join(tid, NULL));
 }
+
+} // namespace
