03c7004cbac3570a2ee665d135e8020476e9437b
donghuixu <donghuixu@jd.com>
2017-12-28 21:24:20 +0800

    fix bazel build travis_ci script

--- build_in_travis_ci.sh
+++ build_in_travis_ci.sh
@@ -11,6 +11,12 @@ if [ -z "$CC" ]; then
     exit 1
 fi
 echo "build combination: PURPOSE=$PURPOSE CXX=$CXX CC=$CC"
+
+if [ "$PURPOSE" = "compile-with-bazel" ]; then
+    bazel build -j8 --copt -DHAVE_ZLIB=1 //...
+    exit 0
+fi
+
 # The default env in travis-ci is Ubuntu.
 if ! sh config_brpc.sh --headers=/usr/include --libs=/usr/lib --nodebugsymbols --cxx=$CXX --cc=$CC; then
     echo "Fail to configure brpc"
@@ -20,8 +26,6 @@ if [ "$PURPOSE" = "compile" ]; then
     make -j4 && sh tools/make_all_examples
 elif [ "$PURPOSE" = "unittest" ]; then
     cd test && sh ./run_tests.sh
-elif [ "$PURPOSE" = "compile-with-bazel" ]; then
-    bazel build --copt -DHAVE_ZLIB=1 //...
 else
     echo "Unknown purpose=\"$PURPOSE\""
 fi
