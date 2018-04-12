d04fe820bd8e43b08f5b117e0e9bdfd110ee5fb5
zhujiashun <zhujiashun@baidu.com>
2018-04-12 17:54:34 +0800

    trim last newline in get command line

--- src/butil/process_util.cc
+++ src/butil/process_util.cc
@@ -70,7 +70,8 @@ ssize_t ReadCommandLine(char* buf, size_t len, bool with_args) {
         return nr;
     } else {
         for (ssize_t i = 0; i < nr; ++i) {
-            if (buf[i] == '\0') {
+            // The result in macos is ended with '\n'
+            if (buf[i] == '\0' || buf[i] == '\n') {
                 return i;
             }
         }
