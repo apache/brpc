eea2303da8cf4dc7ea135113cf03cbd30ad47ecc
wangxuefeng <wangxuefeng@didichuxing.com>
2018-05-24 03:51:19 +0000

    Fix Connection Type issue

--- src/brpc/policy/thrift_protocol.cpp
+++ src/brpc/policy/thrift_protocol.cpp
@@ -638,7 +638,7 @@ void RegisterThriftProtocol() {
                                  policy::SerializeThriftRequest, policy::PackThriftRequest,
                                  policy::ProcessThriftRequest, policy::ProcessThriftResponse,
                                  policy::VerifyThriftRequest, NULL, NULL,
-                                 CONNECTION_TYPE_POOLED, "thrift" };
+                                 CONNECTION_TYPE_POOLED_AND_SHORT, "thrift" };
     if (RegisterProtocol(PROTOCOL_THRIFT, thrift_binary_protocol) != 0) {
         exit(1);
     }
