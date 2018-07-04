4b912063805fcc560b33657c4f7c297e298ee68c
wangxuefeng <wangxuefeng@didichuxing.com>
2018-07-04 06:35:28 +0000

    revert client.cpp and realpath

--- example/echo_c++/client.cpp
+++ example/echo_c++/client.cpp
@@ -22,9 +22,9 @@
 
 DEFINE_string(attachment, "foo", "Carry this along with requests");
 DEFINE_string(protocol, "baidu_std", "Protocol type. Defined in src/brpc/options.proto");
-DEFINE_string(connection_type, "pooled", "Connection type. Available values: single, pooled, short");
+DEFINE_string(connection_type, "", "Connection type. Available values: single, pooled, short");
 DEFINE_string(server, "0.0.0.0:8000", "IP Address of server");
-DEFINE_string(load_balancer, "rr", "The algorithm for load balancing");
+DEFINE_string(load_balancer, "", "The algorithm for load balancing");
 DEFINE_int32(timeout_ms, 100, "RPC timeout in milliseconds");
 DEFINE_int32(max_retry, 3, "Max retries(not including the first RPC)"); 
 DEFINE_int32(interval_ms, 1000, "Milliseconds between consecutive requests");
@@ -44,7 +44,7 @@ int main(int argc, char* argv[]) {
     options.connection_type = FLAGS_connection_type;
     options.timeout_ms = FLAGS_timeout_ms/*milliseconds*/;
     options.max_retry = FLAGS_max_retry;
-    if (channel.Init("file://server.list", FLAGS_load_balancer.c_str(), &options) != 0) {
+    if (channel.Init(FLAGS_server.c_str(), FLAGS_load_balancer.c_str(), &options) != 0) {
         LOG(ERROR) << "Fail to initialize channel";
         return -1;
     }
@@ -77,15 +77,21 @@ int main(int argc, char* argv[]) {
         // the response comes back or error occurs(including timedout).
         stub.Echo(&cntl, &request, &response, NULL);
         if (!cntl.Failed()) {
-            LOG(INFO) << "Received response from " << cntl.remote_side()
-                << " to " << cntl.local_side()
-                << ": " << response.message() << " (attached="
-                << cntl.response_attachment() << ")"
-                << " latency=" << cntl.latency_us() << "us";
+            if (cntl.response_attachment().empty()) {
+                LOG(INFO) << "Received response from " << cntl.remote_side()
+                          << ": " << response.message()
+                          << " latency=" << cntl.latency_us() << "us";
+            } else {
+                LOG(INFO) << "Received response from " << cntl.remote_side()
+                          << " to " << cntl.local_side()
+                          << ": " << response.message() << " (attached="
+                          << cntl.response_attachment() << ")"
+                          << " latency=" << cntl.latency_us() << "us";
+            }
         } else {
             LOG(WARNING) << cntl.ErrorText();
         }
-        //usleep(FLAGS_interval_ms * 1000L);
+        usleep(FLAGS_interval_ms * 1000L);
     }
 
     LOG(INFO) << "EchoClient is going to quit";
