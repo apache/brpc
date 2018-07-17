8e350548101771bc927c680d6639322858aac6f1
Ge Jun <jge666@gmail.com>
2018-07-17 00:04:53 -0700

    minor changes

--- src/brpc/server.cpp
+++ src/brpc/server.cpp
@@ -1032,9 +1032,6 @@ int Server::Stop(int timeout_ms) {
     if (_status != RUNNING) {
         return -1;
     }
-    if (_status != RUNNING) {
-        return -1;
-    }
     _status = STOPPING;
     
     LOG(INFO) << "Server[" << version() << "] is going to quit";
@@ -1375,7 +1372,10 @@ void Server::RemoveMethodsOf(google::protobuf::Service* service) {
             full_name_wo_ns.append(md->name());
             _method_map.erase(full_name_wo_ns);
         }
-
+        if (mp == NULL) {
+            LOG(ERROR) << "Fail to find method=" << md->full_name();
+            continue;
+        }
         if (mp->http_url) {
             butil::StringSplitter at_sp(mp->http_url->c_str(), '@');
             for (; at_sp; ++at_sp) {
@@ -1411,7 +1411,7 @@ void Server::RemoveMethodsOf(google::protobuf::Service* service) {
             delete mp->http_url;
         }
 
-        if (mp != NULL && mp->own_method_status) {
+        if (mp->own_method_status) {
             delete mp->status;
         }
         _method_map.erase(md->full_name());
