d10e4a299755c49890017ac061b5c4a1f01fb5ff
cdjgit <caidaojin@qiyi.com>
2018-10-18 10:59:32 +0800

    fix conflicts

--- src/brpc/controller.cpp
+++ src/brpc/controller.cpp
@@ -256,7 +256,6 @@ void Controller::ResetPods() {
     _request_stream = INVALID_STREAM_ID;
     _response_stream = INVALID_STREAM_ID;
     _remote_stream_settings = NULL;
-    _thrift_method_name = "";
     _couchbase_key_read_replicas = "";
 }
 
