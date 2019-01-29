5389fe47d05e7ce9b6d839211c25988243364c91
eric <eric.buaa.cn@gmail.com>
2019-01-29 13:48:05 +0800

    add include

--- src/bthread/bthread.cpp
+++ src/bthread/bthread.cpp
@@ -16,6 +16,7 @@
 // Author: Ge,Jun (gejun@baidu.com)
 // Date: Tue Jul 10 17:40:58 CST 2012
 
+#include <memory>				// std::unique_ptr
 #include <gflags/gflags.h>
 #include "butil/macros.h"                       // BAIDU_CASSERT
 #include "butil/logging.h"
