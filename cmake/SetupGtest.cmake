2b96d08e94cb116bebcd4189273c6e288b7ed270
zhujiashun <zhujiashun2010@gmail.com>
2019-10-25 18:01:21 +0800

    insert_apache_header

--- cmake/SetupGtest.cmake
+++ cmake/SetupGtest.cmake
@@ -1,4 +1,3 @@
-#
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
 # this work for additional information regarding copyright ownership.
@@ -13,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
 # Setup googletest
 configure_file("${PROJECT_SOURCE_DIR}/cmake/CMakeLists.download_gtest.in" ${PROJECT_BINARY_DIR}/googletest-download/CMakeLists.txt)
