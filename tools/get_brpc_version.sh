ab78a86ddbaa29ff21f5c1d36d9f47cb14d1a1d1
zhujiashun <zhujiashun2010@gmail.com>
2019-12-05 20:07:35 +0800

    fix_missing_brpc_version_in_release: change delimeter to '|'

--- tools/get_brpc_version.sh
+++ tools/get_brpc_version.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 output=$(cat $1/RELEASE_VERSION)
-version=$(git log -1 --format="%h@%cI" 2> /dev/null)
+version=$(git log -1 --format="%h\\|%cI" 2> /dev/null)
 branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
 if [ $? -eq 0 ]
 then
-    output=$output-$branch/$version
+    output=$output"\\|"$branch"\\|"$version
 fi
 echo $output
