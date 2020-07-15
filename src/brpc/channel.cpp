4f92e064db079b42064d9ac4b9a110f7d1567a2c
heyuyi0906 <heyuyi0906@163.com>
2020-07-15 18:42:30 +0800

    refine code

--- src/brpc/channel.cpp
+++ src/brpc/channel.cpp
@@ -468,19 +468,16 @@ void Channel::CallMethod(const google::protobuf::MethodDescriptor* method,
     // possible executions, including:
     //   HandleSendFailed => OnVersionedRPCReturned => IssueRPC(pack_request)
     _serialize_request(&cntl->_request_buf, cntl, request);
+    if (cntl->FailedInline()) {
+        // Handle failures caused by serialize_request, and these error_codes
+        // should be excluded from the retry_policy.
+        return cntl->HandleSendFailed();
+    }
     if (FLAGS_usercode_in_pthread &&
         done != NULL &&
         TooManyUserCode()) {
         cntl->SetFailed(ELIMIT, "Too many user code to run when "
                         "-usercode_in_pthread is on");
-        return cntl->HandleSendFailed();
-    }
-    if (cntl->FailedInline()) {
-        // probably failed before RPC, not called until all necessary
-        // parameters in `cntl' are set.
-        return cntl->HandleSendFailed();
-    }
-    if (cntl->FailedInline()) {
         return cntl->HandleSendFailed();
     }
 
