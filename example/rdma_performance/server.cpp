d353752e33df955a82c2fbda8801094006734036
fan <yfan3763@gmail.com>
2022-11-16 11:51:33 +0000

    update

--- example/rdma_performance/server.cpp
+++ example/rdma_performance/server.cpp
@@ -15,13 +15,14 @@
 // specific language governing permissions and limitations
 // under the License.
 
-#include "brpc/server.h"
+
+#include <gflags/gflags.h>
 #include "butil/atomicops.h"
 #include "butil/logging.h"
 #include "butil/time.h"
+#include "brpc/server.h"
 #include "bvar/variable.h"
 #include "test.pb.h"
-#include <gflags/gflags.h>
 
 #ifdef BRPC_WITH_RDMA
 
@@ -33,63 +34,64 @@ butil::atomic<uint64_t> g_last_time(0);
 namespace test {
 class PerfTestServiceImpl : public PerfTestService {
 public:
-  PerfTestServiceImpl() {}
-  ~PerfTestServiceImpl(){};
+    PerfTestServiceImpl() {}
+    ~PerfTestServiceImpl() {};
 
-  void Test(google::protobuf::RpcController *cntl_base,
-            const PerfTestRequest *request, PerfTestResponse *response,
-            google::protobuf::Closure *done) {
-    brpc::ClosureGuard done_guard(done);
-    uint64_t last = g_last_time.load(butil::memory_order_relaxed);
-    uint64_t now = butil::monotonic_time_us();
-    if (now > last && now - last > 100000) {
-      if (g_last_time.exchange(now, butil::memory_order_relaxed) == last) {
-        response->set_cpu_usage(
-            bvar::Variable::describe_exposed("process_cpu_usage"));
-      } else {
-        response->set_cpu_usage("");
-      }
-    } else {
-      response->set_cpu_usage("");
+    void Test(google::protobuf::RpcController* cntl_base,
+              const PerfTestRequest* request,
+              PerfTestResponse* response,
+              google::protobuf::Closure* done) {
+        brpc::ClosureGuard done_guard(done);
+        uint64_t last = g_last_time.load(butil::memory_order_relaxed);
+        uint64_t now = butil::monotonic_time_us();
+        if (now > last && now - last > 100000) {
+            if (g_last_time.exchange(now, butil::memory_order_relaxed) == last) {
+                response->set_cpu_usage(bvar::Variable::describe_exposed("process_cpu_usage"));
+            } else {
+                response->set_cpu_usage("");
+            }
+        } else {
+            response->set_cpu_usage("");
+        }
+        if (request->echo_attachment()) {
+            brpc::Controller* cntl =
+                static_cast<brpc::Controller*>(cntl_base);
+            cntl->response_attachment().append(cntl->request_attachment());
+        }
     }
-    if (request->echo_attachment()) {
-      brpc::Controller *cntl = static_cast<brpc::Controller *>(cntl_base);
-      cntl->response_attachment().append(cntl->request_attachment());
-    }
-  }
 };
-} // namespace test
+}
 
-int main(int argc, char *argv[]) {
-  GFLAGS_NS::ParseCommandLineFlags(&argc, &argv, true);
+int main(int argc, char* argv[]) {
+    GFLAGS_NS::ParseCommandLineFlags(&argc, &argv, true);
 
-  brpc::Server server;
-  test::PerfTestServiceImpl perf_test_service_impl;
+    brpc::Server server;
+    test::PerfTestServiceImpl perf_test_service_impl;
 
-  if (server.AddService(&perf_test_service_impl,
-                        brpc::SERVER_DOESNT_OWN_SERVICE) != 0) {
-    LOG(ERROR) << "Fail to add service";
-    return -1;
-  }
-  g_last_time.store(0, butil::memory_order_relaxed);
+    if (server.AddService(&perf_test_service_impl, 
+                          brpc::SERVER_DOESNT_OWN_SERVICE) != 0) {
+        LOG(ERROR) << "Fail to add service";
+        return -1;
+    }
+    g_last_time.store(0, butil::memory_order_relaxed);
 
-  brpc::ServerOptions options;
-  options.use_rdma = FLAGS_use_rdma;
-  if (server.Start(FLAGS_port, &options) != 0) {
-    LOG(ERROR) << "Fail to start EchoServer";
-    return -1;
-  }
+    brpc::ServerOptions options;
+    options.use_rdma = FLAGS_use_rdma;
+    if (server.Start(FLAGS_port, &options) != 0) {
+        LOG(ERROR) << "Fail to start EchoServer";
+        return -1;
+    }
 
-  server.RunUntilAskedToQuit();
-  return 0;
+    server.RunUntilAskedToQuit();
+    return 0;
 }
 
 #else
 
-int main(int argc, char *argv[]) {
-  LOG(ERROR) << " brpc is not compiled with rdma. To enable it, please refer to "
-         "https://github.com/apache/incubator-brpc/blob/master/docs/en/rdma.md";
-  return 0;
+
+int main(int argc, char* argv[]) {
+    LOG(ERROR) << " brpc is not compiled with rdma. To enable it, please refer to https://github.com/apache/incubator-brpc/blob/master/docs/en/rdma.md";
+    return 0;
 }
 
 #endif
