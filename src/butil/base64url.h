ddf159e1fd7154aac729ba7dd26d173e8c8a584c
Bright Chen <chenguangmingfe@foxmail.com>
2023-08-02 22:31:49 +0800

    Update base64url license

--- src/butil/base64url.h
+++ src/butil/base64url.h
@@ -1,6 +1,6 @@
-// Copyright 2015 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// found in the LICENSE file
 
 #ifndef BASE_BASE64URL_H_
 #define BASE_BASE64URL_H_
