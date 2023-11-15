806f4d46c9393f6aeb92aa558d95fe95d1784355
Bright Chen <chenguangmingfe@foxmail.com>
2023-11-15 11:39:54 +0800

    Fix comment of http parser and some typos (#2445)

--- src/brpc/details/http_parser.h
+++ src/brpc/details/http_parser.h
@@ -208,7 +208,9 @@ struct http_parser {
   unsigned int index : 8;        /* index into current matcher */
 
   uint32_t nread;          /* # bytes read in various scenarios */
-  uint64_t content_length; /* # bytes in body (0 if no Content-Length header) */
+  uint64_t content_length; /* # bytes in body. `(uint64_t) -1` (all bits one)
+                            * if no Content-Length header.
+                            */
 
   /** READ-ONLY **/
   unsigned short http_major;
