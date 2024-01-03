8322d715c906082a1dc1b7229f96099ba87f072c
扬宁 <yangning.lzy@alibaba-inc.com>
2024-01-03 14:10:49 +0800

    remove if to make operation close

--- src/butil/memory/ref_counted.h
+++ src/butil/memory/ref_counted.h
@@ -285,17 +285,15 @@ class scoped_refptr {
       ptr_->AddRef();
   }
 
-  scoped_refptr(scoped_refptr<T>&& r) noexcept : ptr_(r.ptr_) {
-    if (r.ptr_){
-      r.ptr_ = nullptr;
-    }
+  scoped_refptr(scoped_refptr<T>&& r) noexcept {
+    ptr_ = r.ptr_;
+    r.ptr_ = nullptr;
   }
 
   template <typename U>
-  scoped_refptr(scoped_refptr<U>&& r) noexcept : ptr_(r.ptr_) {
-    if (r.ptr_){
-      r.ptr_ = nullptr;
-    }
+  scoped_refptr(scoped_refptr<U>&& r) noexcept {
+    ptr_ = r.ptr_;
+    r.ptr_ = nullptr;
   }
 
   ~scoped_refptr() {
