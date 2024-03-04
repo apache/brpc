7ca7c286f09306bbcfb63342a3f4035f210aa2b5
MJY-HUST <nn9902@qq.com>
2024-03-04 10:36:41 +0800

    fix_using_butex_in_return_keytable

--- src/bthread/task_group.cpp
+++ src/bthread/task_group.cpp
@@ -307,9 +307,6 @@ void TaskGroup::task_runner(intptr_t skip_remained) {
             thread_return = e.value();
         }
 
-        // Group is probably changed
-        g =  BAIDU_GET_VOLATILE_THREAD_LOCAL(tls_task_group);
-
         // TODO: Save thread_return
         (void)thread_return;
 
@@ -331,6 +328,9 @@ void TaskGroup::task_runner(intptr_t skip_remained) {
             tls_bls.keytable = NULL;
             m->local_storage.keytable = NULL; // optional
         }
+
+        // Group is probably changed
+        g =  BAIDU_GET_VOLATILE_THREAD_LOCAL(tls_task_group);
 
         // Increase the version and wake up all joiners, if resulting version
         // is 0, change it to 1 to make bthread_t never be 0. Any access
