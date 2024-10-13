fb2a47783dc9804c76b9e4d5b4bc32a95029d916
Xiaofeng Wang <wasphin@gmail.com>
2024-10-14 01:10:50 +0800

    Simplify EspMessage implementation by derived from NonreflectableMessage

--- src/brpc/esp_message.cpp
+++ src/brpc/esp_message.cpp
@@ -17,22 +17,14 @@
 
 #include "esp_message.h"
 
-#include <google/protobuf/reflection_ops.h>     // ReflectionOps::Merge
-#include <google/protobuf/wire_format.h>        // WireFormatLite::GetTagWireType
-
+#include "brpc/proto_base.pb.h"
 #include "butil/logging.h"
 
 namespace brpc {
 
 EspMessage::EspMessage()
-    : ::google::protobuf::Message() {
+    : MessageHelper<EspMessage>::BaseType() {
     SharedCtor();
-}
-
-EspMessage::EspMessage(const EspMessage& from)
-    : ::google::protobuf::Message() {
-    SharedCtor();
-    MergeFrom(from);
 }
 
 void EspMessage::SharedCtor() {
@@ -50,81 +42,19 @@ const ::google::protobuf::Descriptor* EspMessage::descriptor() {
     return EspMessageBase::descriptor();
 }
 
-EspMessage* EspMessage::New() const {
-    return new EspMessage;
-}
-
-#if GOOGLE_PROTOBUF_VERSION >= 3006000
-EspMessage* EspMessage::New(::google::protobuf::Arena* arena) const {
-    return CreateMaybeMessage<EspMessage>(arena);
-}
-#endif
-
 void EspMessage::Clear() {
     head.body_len = 0;
     body.clear();
 }
 
-bool EspMessage::MergePartialFromCodedStream(
-        ::google::protobuf::io::CodedInputStream* input) {
-#define DO_(EXPRESSION) if (!(EXPRESSION)) return false
-    ::google::protobuf::uint32 tag;
-
-    while ((tag = input->ReadTag()) != 0) {
-        if (::google::protobuf::internal::WireFormatLite::GetTagWireType(tag) ==
-                ::google::protobuf::internal::WireFormatLite::WIRETYPE_END_GROUP) {
-            return true;
-        }
-    }
-    return true;
-#undef DO_
-}
-
-void EspMessage::SerializeWithCachedSizes(
-        ::google::protobuf::io::CodedOutputStream*) const {
-}
-
-::google::protobuf::uint8* EspMessage::SerializeWithCachedSizesToArray(
-        ::google::protobuf::uint8* target) const {
-    return target;
-}
-
-int EspMessage::ByteSize() const {
+size_t EspMessage::ByteSizeLong() const {
     return sizeof(head) + body.size();
-}
-
-void EspMessage::MergeFrom(const ::google::protobuf::Message& from) {
-    CHECK_NE(&from, this);
-    const EspMessage* source = dynamic_cast<const EspMessage*>(&from);
-    if (source == NULL) {
-        ::google::protobuf::internal::ReflectionOps::Merge(from, this);
-    } else {
-        MergeFrom(*source);
-    }
 }
 
 void EspMessage::MergeFrom(const EspMessage& from) {
     CHECK_NE(&from, this);
     head = from.head;
     body = from.body;
-}
-
-void EspMessage::CopyFrom(const ::google::protobuf::Message& from) {
-    if (&from == this) {
-        return;
-    }
-
-    Clear();
-    MergeFrom(from);
-}
-
-void EspMessage::CopyFrom(const EspMessage& from) {
-    if (&from == this) {
-        return;
-    }
-
-    Clear();
-    MergeFrom(from);
 }
 
 bool EspMessage::IsInitialized() const {
@@ -138,13 +68,6 @@ void EspMessage::Swap(EspMessage* other) {
         head = tmp;
         body.swap(other->body);
     }
-}
-
-::google::protobuf::Metadata EspMessage::GetMetadata() const {
-    ::google::protobuf::Metadata metadata;
-    metadata.descriptor = EspMessage::descriptor();
-    metadata.reflection = NULL;
-    return metadata;
 }
 
 } // namespace brpc
