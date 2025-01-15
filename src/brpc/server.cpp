2c9e0b0c3ce80e0b26d6c56bb2915b5bb904a908
w-gc <25614556+w-gc@users.noreply.github.com>
2025-01-15 17:53:37 +0800

    fix memory leak issue

--- src/brpc/server.cpp
+++ src/brpc/server.cpp
@@ -424,7 +424,7 @@ Server::Server(ProfilerLinker)
     , _eps_bvar(&_nerror_bvar)
     , _concurrency(0)
     , _concurrency_bvar(cast_no_barrier_int, &_concurrency)
-    ,_has_progressive_read_method(false) {
+    , _has_progressive_read_method(false) {
     BAIDU_CASSERT(offsetof(Server, _concurrency) % 64 == 0,
                   Server_concurrency_must_be_aligned_by_cacheline);
 }
@@ -782,6 +782,47 @@ static bool OptionsAvailableOverRdma(const ServerOptions* opt) {
 static AdaptiveMaxConcurrency g_default_max_concurrency_of_method(0);
 static bool g_default_ignore_eovercrowded(false);
 
+
+inline void copy_server_option(ServerOptions& dst, const ServerOptions& src) {
+    if (&dst == &src) {
+        return;
+    }
+
+#define FREE_PTR_IF_NOT_REUSED(ptr)         \
+    if (dst.ptr != src.ptr) {               \
+        delete dst.ptr;                     \
+        dst.ptr = NULL;                     \
+    }
+
+    FREE_PTR_IF_NOT_REUSED(nshead_service);
+
+#ifdef ENABLE_THRIFT_FRAMED_PROTOCOL
+    FREE_PTR_IF_NOT_REUSED(thrift_service);
+#endif
+
+    FREE_PTR_IF_NOT_REUSED(baidu_master_service);
+    FREE_PTR_IF_NOT_REUSED(http_master_service);
+    FREE_PTR_IF_NOT_REUSED(rpc_pb_message_factory);
+
+    if (dst.pid_file != src.pid_file && !dst.pid_file.empty()) {
+        unlink(dst.pid_file.c_str());
+    }
+
+    if (dst.server_owns_auth) {
+        FREE_PTR_IF_NOT_REUSED(auth);
+    }
+
+    if (dst.server_owns_interceptor) {
+        FREE_PTR_IF_NOT_REUSED(interceptor);
+    }
+
+    FREE_PTR_IF_NOT_REUSED(redis_service);
+
+#undef FREE_PTR_IF_NOT_REUSED
+
+    dst = src;
+}
+
 int Server::StartInternal(const butil::EndPoint& endpoint,
                           const PortRange& port_range,
                           const ServerOptions *opt) {
@@ -814,22 +855,22 @@ int Server::StartInternal(const butil::EndPoint& endpoint,
         return -1;
     }
 
-    if (&_options == opt) {
-        // do nothing
-    } else if (opt) {
-        if (_options.rpc_pb_message_factory != opt->rpc_pb_message_factory) {
-            delete _options.rpc_pb_message_factory;
-            _options.rpc_pb_message_factory = NULL;
-        }
-
-        _options = *opt;
+    if (opt) {
+        copy_server_option(_options, *opt);
     } else {
+        // Don't forget to release `rpc_pb_message_factory` before overwriting `_options`
         delete _options.rpc_pb_message_factory;
         _options.rpc_pb_message_factory = NULL;
 
         // Always reset to default options explicitly since `_options'
         // may be the options for the last run or even bad options
         _options = ServerOptions();
+    }
+
+    // Create the resource if:
+    //   1. `_options` copied from user and user forgot to create
+    //   2. `_options` created by our
+    if (!_options.rpc_pb_message_factory) {
         _options.rpc_pb_message_factory = new DefaultRpcPBMessageFactory();
     }
 
