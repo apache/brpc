15ad84ff7f6e7f67b13833c11b141a6d2a80e393
gitccl <chenchuanle6@gmail.com>
2025-06-08 00:41:24 +0800

    fix ut

--- src/bthread/timer_thread.cpp
+++ src/bthread/timer_thread.cpp
@@ -347,8 +347,8 @@ void TimerThread::run() {
         // would run the consumed tasks.
         {
             BAIDU_SCOPED_LOCK(_mutex);
-            if (__builtin_expect(_nearest_run_time == 0, 0)) {
-                // If _nearest_run_time is 0, it means that stop_and_join() was called
+            if (__builtin_expect(_nearest_run_time == -1, 0)) {
+                // If _nearest_run_time is -1, it means that stop_and_join() was called
                 CHECK(_stop.load(butil::memory_order_relaxed));
                 break;
             }
@@ -447,7 +447,7 @@ void TimerThread::stop_and_join() {
         {
             BAIDU_SCOPED_LOCK(_mutex);
              // trigger pull_again and wakeup TimerThread
-            _nearest_run_time = 0;
+            _nearest_run_time = -1;
             ++_nsignals;
         }
         if (pthread_self() != _thread) {
