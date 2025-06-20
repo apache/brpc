67057f8fe602b20f4dbd6f69f2ba57e1c1d586c9
Yang,Liming <liming.yang@139.com>
2025-06-20 14:11:36 +0800

    rdma support polling mode (#2920)

--- src/brpc/socket.cpp
+++ src/brpc/socket.cpp
@@ -2275,6 +2275,14 @@ int Socket::OnInputEvent(void* user_data, uint32_t events,
         attr.tag = bthread_self_tag();
         if (FLAGS_usercode_in_coroutine) {
             ProcessEvent(p);
+#if BRPC_WITH_RDMA
+        } else if (rdma::FLAGS_rdma_edisp_unsched == false) {
+            auto rc = bthread_start_background(&tid, &attr, ProcessEvent, p);
+            if (rc != 0) {
+                LOG(FATAL) << "Fail to start ProcessEvent";
+                ProcessEvent(p);
+            }
+#endif
         } else if (bthread_start_urgent(&tid, &attr, ProcessEvent, p) != 0) {
             LOG(FATAL) << "Fail to start ProcessEvent";
             ProcessEvent(p);
