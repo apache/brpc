f03e0f6fa6227ddf2e61da7124a1745958a4b8af
giriraj-singh-couchbase <giriraj.singh@couchbase.com>
2025-10-02 23:16:54 +0530

    fixed some issue.

--- src/brpc/couchbase.cpp
+++ src/brpc/couchbase.cpp
@@ -32,7 +32,7 @@ namespace brpc {
 
 // Couchbase protocol constants
 namespace {
-  constexpr uint32_t APPLE_VBUCKET_COUNT = 64;
+  [[maybe_unused]] constexpr uint32_t APPLE_VBUCKET_COUNT = 64;
   constexpr uint32_t DEFAULT_VBUCKET_COUNT = 1024;
   constexpr int CONNECTION_ID_SIZE = 33;
   constexpr size_t RANDOM_ID_HEX_SIZE = 67; // 33 bytes * 2 + null terminator
